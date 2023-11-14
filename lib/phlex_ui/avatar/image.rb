c1accb50e574b4237f2e1622fbc095d9042b7424
george kettle <georgekettle.gk@gmail.com>
2023-11-14 12:09:35 +1100

    Avatar component

---
+++ lib/phlex_ui/avatar/image.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module PhlexUI
+    class Avatar::Image < Base
+        def initialize(src:, alt: '', **attrs)
+            @src = src
+            @alt = alt
+            super(**attrs)
+        end
+
+        def template
+            img(**attrs)
+        end
+
+        private
+
+        def default_attrs
+            {
+                loading: "lazy",
+                class: "aspect-square h-full w-full",
+                alt: @alt,
+                src: @src
+            }
+        end
+    end
+end
