59c84f196df40e3fb36662e30676231a81772547
george kettle <georgekettle.gk@gmail.com>
2023-11-16 18:28:11 +1100

    HoverCard

---
+++ lib/phlex_ui/hover_card.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module PhlexUI
+    class HoverCard < Base
+        def initialize(option: {}, **attrs)
+            @options = option
+            @options[:delay] ||= [500, 250]
+            @options[:trigger] ||= 'mouseenter focus click'
+            super(**attrs)
+        end
+
+        def template(&)
+            div(**attrs, &)
+        end
+
+        private
+
+        def default_attrs
+            {
+                data: {
+                    controller: "popover",
+                    popover_options_value: @options.to_json
+                }
+            }
+        end
+    end
+end
