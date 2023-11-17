dfdc5e4eeeb74d08059e466df2d08dc19ec20ddb
george kettle <georgekettle.gk@gmail.com>
2023-11-17 18:21:40 +1100

    Fixed PhlexUI::ShortcutKey

--- lib/phlex_ui/shortcut_key.rb
+++ lib/phlex_ui/shortcut_key.rb
@@ -8,7 +8,7 @@ module PhlexUI
 
         private
 
-        def default_attributes
+        def default_attrs
             {
                 class: "pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted-background px-1.5 font-mono text-[10px] font-medium text-muted-text opacity-100"
             }
