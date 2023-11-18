75d3cb424e1126a68ea12c49faafab6dfdbfbb79
george kettle <georgekettle.gk@gmail.com>
2023-11-18 18:46:03 +1100

    Tooltip

---
+++ lib/phlex_ui/tooltip.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module PhlexUI
+    class Tooltip < Base
+        def initialize(options: {}, **attrs)
+            @options = options
+            super(**attrs)
+        end
+
+        def template(&)
+            div(**attrs, &)
+        end
+
+        private
+
+        def default_attrs
+            {
+                data: {
+                    controller: "popover",
+                    popover_options_value: @options.to_json
+                }
+            }
+        end
+    end
+end
