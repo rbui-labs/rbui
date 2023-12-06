6be5037de8433c92bea672a9618f0f354867fc62
george kettle <georgekettle.gk@gmail.com>
2023-12-06 12:04:18 +1100

    Updated Accorion view to use Accordion::Builder

--- lib/phlex_ui/accordion.rb
+++ lib/phlex_ui/accordion.rb
@@ -6,17 +6,6 @@ module PhlexUI
             div(**attrs, &)
         end
 
-        def add_item(title, content, open: false)
-            render Accordion::Item.new(open: open) do
-                render PhlexUI::Accordion::Trigger.new do
-                    render PhlexUI::Accordion::DefaultTrigger.new { title }
-                end
-                render PhlexUI::Accordion::Content.new do
-                    render PhlexUI::Accordion::DefaultContent.new { content }
-                end
-            end
-        end
-
         private
 
         def default_attrs
