d7e7cce779494c14ce5cd99376c80d9027fcd0de
Cirdes <cirdes@linkana.com>
2024-07-31 21:13:39 +0000

    remove comments from rbui.rb

--- lib/rbui.rb
+++ lib/rbui.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
-# loader = Zeitwerk::Loader.for_gem
-
-# loader.inflector.inflect(
-#   "rbui" => "RBUI"
-# )
-
-# loader.collapse("#{__dir__}/rbui/combobox")
-
-# loader.setup # ready!
-
 module RBUI
   extend Phlex::Kit
 end
-
-# loader.eager_load
