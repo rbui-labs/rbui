d7f23ee2103a6404f4adfc67c6f38277a8bf5ebf
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-08-05 03:29:07 -0400

    add entry file and missing package

---
+++ index.js
@@ -0,0 +1,26 @@
+// Load and export all of the stimulus controllers
+import { Application } from "@hotwired/stimulus";
+
+const application = Application.start();
+
+// Configure Stimulus development experience
+application.debug = false;
+window.Stimulus = application;
+
+// Import all controller files
+import ComboboxController from "./lib/rbui/combobox/combobox_controller";
+import ComboboxContentController from "./lib/rbui/combobox/combobox_content_controller";
+import ComboboxItemController from "./lib/rbui/combobox/combobox_item_controller";
+ 
+// Register all controllers
+application.register("rbui--combobox", ComboboxController);
+application.register("rbui--combobox-content", ComboboxContentController);
+application.register("rbui--combobox-item", ComboboxItemController);
+
+// Export all controllers so user of npm package can lazy load controllers
+export { default as ComboboxController } from "./lib/rbui/combobox/combobox_controller";
+export { default as ComboboxContentController } from "./lib/rbui/combobox/combobox_content_controller";
+export { default as ComboboxItemController } from "./lib/rbui/combobox/combobox_item_controller";
+ 
+// Export application
+export { application };
