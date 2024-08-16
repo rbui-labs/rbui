9647c82e71ac5a4048415e570b8f643c240fb845
Jean Pierry de Souza Mendonça <pierrybm@gmail.com>
2024-08-16 10:58:15 -0300

    BETTER SELECT

--- lib/rbui/select/select_item.rb
+++ lib/rbui/select/select_item.rb
@@ -22,7 +22,7 @@ module RBUI
         viewbox: "0 0 24 24",
         fill: "none",
         stroke: "currentColor",
-        class: "invisible group-aria-selected:visible	mr-2 h-4 w-4",
+        class: "invisible group-aria-selected:visible	mr-2 h-4 w-4 flex-none",
         stroke_width: "2",
         stroke_linecap: "round",
         stroke_linejoin: "round"
--- lib/rbui/select/select_trigger.rb
+++ lib/rbui/select/select_trigger.rb
@@ -47,7 +47,7 @@ module RBUI
           activedescendant: true
         },
         class:
-          "w-full flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+          "truncate w-full flex h-9 items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
       }
     end
   end
