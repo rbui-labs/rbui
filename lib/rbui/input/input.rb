9de6b3e33aa1a59443536c88f5ed043bc0056464
Cirdes Henrique <cirdes@gmail.com>
2024-08-24 16:43:10 -0300

    fix input

--- lib/rbui/input/input.rb
+++ lib/rbui/input/input.rb
@@ -15,6 +15,10 @@ module RBUI
 
     def default_attrs
       {
+        data: {
+          rbui__form_field_target: "input",
+          action: "input->rbui--form-field#onInput invalid->rbui--form-field#onInvalid"
+        },
         class: "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50"
       }
     end
