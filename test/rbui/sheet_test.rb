89a379eb10466acf474f46d10a8481eae5803724
Jean Pierry de Souza Mendonça <pierrybm@gmail.com>
2024-08-29 17:10:54 -0300

    MINITEST

--- test/rbui/sheet_test.rb
+++ test/rbui/sheet_test.rb
@@ -2,25 +2,26 @@
 
 require "test_helper"
 
-class PhlexUI::SheetTest < Minitest::Test
+class RBUI::SheetTest < Minitest::Test
   include Phlex::Testing::ViewHelper
 
   def test_render_with_all_items
     output = phlex_context do
-      PhlexUI.Sheet do
-        PhlexUI.SheetTrigger do
+      RBUI.Sheet do
+        RBUI.SheetTrigger do
           RBUI.Button(variant: :outline) { "Open Sheet" }
         end
-        PhlexUI.SheetContent(class: "sm:max-w-sm") do
-          PhlexUI.SheetHeader do
-            PhlexUI.SheetTitle { "Edit profile" }
-            PhlexUI.SheetDescription { "Make changes to your profile here. Click save when you're done." }
+
+        RBUI.SheetContent(class: "sm:max-w-sm") do
+          RBUI.SheetHeader do
+            RBUI.SheetTitle { "Edit profile" }
+            RBUI.SheetDescription { "Make changes to your profile here. Click save when you're done." }
           end
-          PhlexUI.SheetMiddle do
+          RBUI.SheetMiddle do
             RBUI.Input(placeholder: "Joel Drapper") { "Joel Drapper" }
             RBUI.Input(placeholder: "joel@drapper.me")
 
-            PhlexUI.SheetFooter do
+            RBUI.SheetFooter do
               RBUI.Button(variant: :outline, data: {action: "click->rbui--sheet-content#close"}) { "Cancel" }
               RBUI.Button(type: "submit") { "Save" }
             end
