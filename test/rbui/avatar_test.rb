f7b5b7cf34278926a677d3c50f1a508d7be495ad
Jackson Pires <jackson.pires@gmail.com>
2024-09-03 08:48:50 -0300

    move avatar to rbui (#105)

--- test/rbui/avatar_test.rb
+++ test/rbui/avatar_test.rb
@@ -7,9 +7,9 @@ class PhlexUI::AvatarTest < Minitest::Test
 
   def test_render_with_all_items
     output = phlex_context do
-      PhlexUI.Avatar do
-        PhlexUI.AvatarImage(src: "https://avatars.githubusercontent.com/u/246692?v=4", alt: "joeldrapper")
-        PhlexUI.AvatarFallback { "JD" }
+      RBUI.Avatar do
+        RBUI.AvatarImage(src: "https://avatars.githubusercontent.com/u/246692?v=4", alt: "joeldrapper")
+        RBUI.AvatarFallback { "JD" }
       end
     end
 
