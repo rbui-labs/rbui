a8fd80b7e23e8112aba731700b9c1a5793dde038
Jean Pierry de Souza Mendonça <pierrybm@gmail.com>
2024-09-06 11:35:59 -0300

    Update select_content.rb
    Signed-off-by: Jean Pierry de Souza Mendonça <pierrybm@gmail.com>

--- lib/rbui/select/select_content.rb
+++ lib/rbui/select/select_content.rb
@@ -10,7 +10,7 @@ module RBUI
     def view_template(&block)
       div(**attrs) do
         div(
-          class: "max-h-96 min-w-max overflow-auto rounded-md border bg-background p-1 text-foreground shadow-md animate-out group-data-[rbui--select-open-value=true]/select:animate-in fade-out-0 group-data-[rbui--select-open-value=true]/select:fade-in-0 zoom-out-95 group-data-[rbui--select-open-value=true]/select:zoom-in-95 slide-in-from-top-2", &block
+          class: "fixed max-h-96 min-w-max overflow-auto rounded-md border bg-background p-1 text-foreground shadow-md animate-out group-data-[rbui--select-open-value=true]/select:animate-in fade-out-0 group-data-[rbui--select-open-value=true]/select:fade-in-0 zoom-out-95 group-data-[rbui--select-open-value=true]/select:zoom-in-95 slide-in-from-top-2", &block
         )
       end
     end
