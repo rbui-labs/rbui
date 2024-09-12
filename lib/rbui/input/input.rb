a6a5f83c9c8f10742b84726d2a5519e00d2eca10
Jackson Pires <jackson.pires@gmail.com>
2024-09-12 16:59:56 -0300

    add cursor pointer to input when type is file

--- lib/rbui/input/input.rb
+++ lib/rbui/input/input.rb
@@ -13,13 +13,27 @@ module RBUI
 
     private
 
+    def default_classes_by_type
+      return file_classes if @type == :file
+
+      default_classes
+    end
+
+    def default_classes
+      "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 border-border focus-visible:ring-ring placeholder:text-muted-foreground"
+    end
+
+    def file_classes
+      "flex h-9 w-full rounded-md border bg-background px-3 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 border-border focus-visible:ring-ring placeholder:text-muted-foreground hover:file:cursor-pointer py-1.5"
+    end
+
     def default_attrs
       {
         data: {
           rbui__form_field_target: "input",
           action: "input->rbui--form-field#onInput invalid->rbui--form-field#onInvalid"
         },
-        class: "flex h-9 w-full rounded-md border bg-background px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 border-border focus-visible:ring-ring placeholder:text-muted-foreground"
+        class: default_classes_by_type
       }
     end
   end
