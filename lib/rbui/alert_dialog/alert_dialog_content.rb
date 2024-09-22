91bafe44d14ff608ac07202d9f497239cb7d99c4
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-09-22 13:14:26 +0200

    remove template tag

--- lib/rbui/alert_dialog/alert_dialog_content.rb
+++ lib/rbui/alert_dialog/alert_dialog_content.rb
@@ -3,7 +3,7 @@
 module RBUI
   class AlertDialogContent < Base
     def view_template(&block)
-      template_tag(**attrs) do
+      div(**attrs) do
         div(data: {controller: "rbui--alert-dialog"}) do
           background
           container(&block)
@@ -14,8 +14,10 @@ module RBUI
     def background
       div(
         data_state: "open",
-        class:
-              "fixed inset-0 z-50 bg-black/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        class: [
+          "fixed inset-0 z-50 bg-black/80 backdrop-blur-sm data-[state=open]:animate-in",
+          "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0"
+        ],
         style: "pointer-events:auto",
         data_aria_hidden: "true",
         aria_hidden: "true"
@@ -36,6 +38,7 @@ module RBUI
 
     def default_attrs
       {
+        class: "hidden",
         data: {
           rbui__alert_dialog_target: "content"
         }
