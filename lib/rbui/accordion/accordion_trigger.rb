f6129a98df52dec5fee6aa7ecc628bf68f54c89b
Jean Pierry de Souza Mendonça <pierrybm@gmail.com>
2024-10-23 11:22:31 -0300

    1st OK (#167)

--- lib/rbui/accordion/accordion_trigger.rb
+++ lib/rbui/accordion/accordion_trigger.rb
@@ -8,6 +8,7 @@ module RBUI
 
     def default_attrs
       {
+        type: "button",
         data: {action: "click->rbui--accordion#toggle"},
         class: "w-full flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline"
       }
